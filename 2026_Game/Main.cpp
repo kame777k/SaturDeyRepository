9f522332a2881b5936dd86dd4f36860661d179da
kame777k <kumamon3312@gmail.com>
2026-07-04 10:40:37 +0900

    初期化

---
+++ 2026_Game/Main.cpp
@@ -0,0 +1,20 @@
+#include"DxLib.h"
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	ChangeWindowMode(TRUE);
+	DxLib_Init();
+	SetGraphMode(800, 600, 32);
+	if (DxLib_Init() == -1)return -1;
+	
+	int fontColor = GetColor(255, 100, 0);
+
+	while(ProcessMessage()== 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
+	{
+		ClearDrawScreen();
+
+		SetFontSize(120);
+		DrawFormatString(75, 250, fontColor, "Hello, DxLib!");
+		ScreenFlip();
+	}
+}
